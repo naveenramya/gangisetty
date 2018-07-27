8b10b0c381acb171e3c9ba35c0c2b5a1702b5c25
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-07-27 15:20:25 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x;
+		System.out.println("enter an integer to check if it is odd or even");
+		Scanner sc=new Scanner(System.in);
+		x=sc.nextInt();
+		if(x % 2==0)
+		System.out.println("you entered an even number");
+		else
+		{
+			System.out.println("you entered an odd number");
+		}
+		
+		
+	
+	
+}}
