41998ae8d88852171ff4c02bac2de20f87a22672
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-09 12:00:40 +0530

    Create sum of k integers.java

---
+++ sum of k integers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ideone
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int k=sc.nextInt();
+   int[]a=new int[19];
+   int sum=0;
+   for(int i=0;i<n;i++)
+            {
+	   a[i]=sc.nextInt();
+          }
+	for(int i=0;i<k;i++)
+              {
+	sum+=a[i];
+              }
+	System.out.println(sum);
+}	
+}
