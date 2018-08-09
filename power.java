44e5de66960a31bd5ac00a289125921259e40d69
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-09 12:54:42 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+              int base = 3, exponent = -4;
+              double result = Math.pow(base, exponent);
+               System.out.println("Answer = " + result);
+    }
+}
