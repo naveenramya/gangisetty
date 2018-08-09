72cc9d1b6b00a32ce6e5e79a14f7c197366909ca
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-09 12:32:27 +0530

    Create largest number.java

---
+++ largest number.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Ideone
+{
+  public static void main(String args[])
+  {
+   Long year;
+    Scanner input= new Scanner(System.in);
+       year= input.nextLong();
+        if(year%4==0)
+        {
+           System.out.println("yes");
+        }
+        else
+        {
+             System.out.println("no");
+        }
+  }
+}
+
+
+	
